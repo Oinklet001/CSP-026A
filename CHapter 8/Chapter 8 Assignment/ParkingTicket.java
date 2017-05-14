567fe017443b2c1d9f7d9339c6f751712f1c0f25
Oinklet001 <trwer1422@gmail.com>
2017-05-14 01:16:59 -0700

    Update ParkingTicket.java

--- CHapter 8/Chapter 8 Assignment/ParkingTicket.java
+++ CHapter 8/Chapter 8 Assignment/ParkingTicket.java
@@ -1,25 +1,25 @@
 public class ParkingTicket {
-	
-	//Making a refrence
-	private ParkedCar carPs;
-	
-	private PoliceOfficer policePs;
-	
-	private ParkingMeter parkedMS;
-	
-	
+    
+    //Making a refrence
+    private ParkedCar carPs;
+    
+    private PoliceOfficer policePs;
+    
+    private ParkingMeter parkedMS;
+    
+    
 
-	
-	public ParkingTicket(ParkedCar a, PoliceOfficer b, ParkingMeter c){
-		this.carPs=a;
-		this.policePs=b;
-		this.parkedMS=c;
+    
+    public ParkingTicket(ParkedCar a, PoliceOfficer b, ParkingMeter c){
+        this.carPs=a;
+        this.policePs=b;
+        this.parkedMS=c;
 
-	}
-	
-	  public String toString() {
-			return "The fine is " +  policePs.getCalculateFine  + ". From officer " + policePs.getcopName() + " badge number " + policePs.getbadgeNum()
-					+ ". The car make, model, color, and licinse number is " + carPs.getMake() +", " + carPs.getModel() + ", " +
-					carPs.getColor() + ", " + carPs.getLicenseNum() + ".";
-		}
+    }
+    
+      public String toString() {
+            return "The fine is " +  policePs.getCalculatedFine(carPs,parkedMS)  + ". From officer " + policePs.getcopName() + " badge number " + policePs.getbadgeNum()
+                    + ". The car make, model, color, and licinse number is " + carPs.getMake() +", " + carPs.getModel() + ", " +
+                    carPs.getColor() + ", " + carPs.getLicenseNum() + ".";
+        }
 }
